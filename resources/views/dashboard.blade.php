8691528b565e7896e6974d2a18ab3656e2132f55
yhya82 <gassamay78@gmail.com>
2026-03-18 14:26:06 -0700

    dashboard api created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,45 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+
+<div>
+    <div >
+        <div>
+            <p>Total Revenue</p>
+            <p id="total_revenue"></p>
+        </div>
+        <div>
+            <p>Monthly Revenue</p>
+            <p id ='monthly_revenue'></p>
+        </div>
+        <div>
+            <p>Total Members</p>
+            <p id='total_members'></p>
+        </div>
+        <div>
+            <p>Active Members</p>
+            <p id="active_members"></p>
+        </div>
+        <div>
+            <p>Expired Members</p>
+            <p id="expired_members"></p>
         </div>
     </div>
-</x-app-layout>
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function(){
+
+        fetch(`/api/dashboard`)
+        .then(res => res.json())
+        .then(data => {
+
+            document.getElementById('total_revenue').innerText = data.total_revenue;
+            document.getElementById('monthly_revenue').innerText = data.monthly_revenue;
+            document.getElementById('total_members').innerText = data.total_members;
+            document.getElementById('active_members').innerText = data.active_members;
+            document.getElementById('expired_members').innerText = data.expired_members;
+
+        })
+
+        .catch(err => console.error(err));
+    })
+</script>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 use App\Http\Controllers\Api\MemberApiController;
+use App\Http\Controllers\Api\DashboardApiController;
+
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('members',MemberApiController::class);
+Route::get('dashboard',[DashboardApiController::class, 'index']);
