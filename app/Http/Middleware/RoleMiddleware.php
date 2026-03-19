5cd2e4a22a1e54ca8cf61d23ec623f491d59ae85
yhya82 <gassamay78@gmail.com>
2026-03-19 11:11:08 -0700

    role midddleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -13,8 +13,14 @@ class RoleMiddleware
      *
      * @param  Closure(Request): (Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $roles): Response
     {
+        if(!auth()->check()){
+            return response()->json(['message'=>'Unauthenticated'],401);
+        }
+        if(!in_array(auth()-user()->role,$roles)){
+            return response()->json(['message'=>'Forbidden'],403);
+        }
         return $next($request);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->alias([
+            'role'=> \App\Http\Middleware\RoleMiddleware::class,
+        ]);
+
         $middleware->api(prepend:[
             EnsureFrontendRequestsAreStateful::class,
         ]);
